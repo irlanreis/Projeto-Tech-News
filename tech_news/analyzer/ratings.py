3820368b0868329e6c0cfde7a57111fe936aa831
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-10-31 23:33:18 -0400

    Fix: Corrige ortografia no teste do req.10; Feat: Finaliza projeto :rocket:

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,23 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    categorys = db.news.find({}, {"category": True})
+    categories_all = {}
+
+    for category in categorys:
+        if categories_all.get(category["category"]):
+            categories_all[category["category"]] += 1
+        else:
+            categories_all[category["category"]] = 1
+
+    categories_sorted = list(
+        dict(
+            sorted(
+                categories_all.items(), key=lambda item: (-item[1], item[0])
+            )
+        ).keys()
+    )
+
+    return categories_sorted[:5]
